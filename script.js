acfdf2b2533281e06adf8caa847cb7405fde2bd1
Wojtek <78815235+s7awh@users.noreply.github.com>
2025-01-12 17:43:05 +0100

    added new comments

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let countG = 0;
 function updateScores() {
     document.getElementById('homeScore').innerText = countH;
     document.getElementById('guestScore').innerText = countG;
-    maxScorehighlight(); // Automatyczne podświetlenie po aktualizacji
+    maxScorehighlight(); // automatic highlight after updating the score
 }
 
 function add1Home () { 
@@ -44,7 +44,7 @@ function add3Guest () {
     updateScores()
 }
 
-function reset() {
+function reset() { // new game button function reseting the scoreboard
     countG = 0;
     countH = 0;
     document.getElementById("homeScore").textContent = countH;
@@ -56,32 +56,14 @@ function maxScorehighlight () {
     const homeScore = document.getElementById('homeScore');
     const guestScore = document.getElementById('guestScore');
 
-    homeScore.classList.remove('highlight');
-    guestScore.classList.remove('highlight');
+    homeScore.classList.remove('highlight'); //removing highligh
+    guestScore.classList.remove('highlight'); //removing highligh
 
     if (countH > countG) {
-        homeScore.classList.add('highlight');        
+        homeScore.classList.add('highlight'); // highlight Home if bigger than Guest   
     } else if (countG > countH) {
-        guestScore.classList.add('highlight');
+        guestScore.classList.add('highlight'); // highlight Guest if bigger than Home
     }
 }
 
-/* 
 
-    function highlightMaxScore(){
-    const homeElement = document.getElementById('homeScore');
-    const guestElement = document.getElementById('guestScore');
-
-    // Usunięcie podświetlenia z obu elementów
-    homeElement.classList.remove('highlight');
-    guestElement.classList.remove('highlight');
-
-    // Porównanie wyników
-    if (countH > countG) {
-        homeElement.classList.add('highlight'); // Podświetlenie HOME
-    } else if (countG > countH) {
-        guestElement.classList.add('highlight'); // Podświetlenie GUEST
-    }
-    } 
-
-*/
